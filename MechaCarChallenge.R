a715d2fedff4b30d545bbdb32d5fd5bffc164869
lee1woo <102992388+lee1woo@users.noreply.github.com>
2022-10-06 20:01:36 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,6 @@
+
+
+# Deliverable 1 
 library(tidyverse)
 
 MechaCar_mpg <- read.csv(file="MechaCar_mpg.csv")
@@ -6,3 +9,16 @@ linreg <- lm(mpg ~vehicle_length + vehicle_weight + spoiler_angle + ground_clear
 
 summary(linreg)
 
+# Deliverable 2
+
+Suspension <- read.csv(file="Suspension_Coil.csv")
+
+total_summary <- summarize(Suspension, Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+total_summary
+
+manufacturing_group = group_by(Suspension, Manufacturing_Lot)
+
+lot_summary <- summarize(manufacturing_group, Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+lot_summary
