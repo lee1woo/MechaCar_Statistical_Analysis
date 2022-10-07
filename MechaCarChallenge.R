4dbdbe71ed84c35c7bce13d2faae0f660345afd7
lee1woo <102992388+lee1woo@users.noreply.github.com>
2022-10-06 20:06:45 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -22,3 +22,16 @@ manufacturing_group = group_by(Suspension, Manufacturing_Lot)
 lot_summary <- summarize(manufacturing_group, Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
 lot_summary
+
+# Deliverable 3
+
+t.test(Suspension$PSI, mu=1500)
+
+ML1 <- subset(Suspension, Manufacturing_Lot == "Lot1")
+t.test(ML1$PSI, mu=1500)
+
+ML2 <- subset(Suspension, Manufacturing_Lot == "Lot2")
+t.test(ML2$PSI, mu=1500)
+
+ML3 <- subset(Suspension, Manufacturing_Lot == "Lot3")
+t.test(ML3$PSI, mu=1500)
